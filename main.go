c659baf3aaa774ca5af1c9536d90a1e3a2c669aa
Megh Rathod <me@meghrathod.dev>
2025-01-26 00:44:36 -0500

    feat: implement creating and deleting URLs

--- main.go
+++ main.go
@@ -1,45 +1,80 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
+	"strings"
 	"time"
 )
 
 type shortURL struct {
-	key         string
-	url         string
-	dateCreated time.Time
-	pin         string
+	ShortURL    string    `json:"shortURL"`
+	Url         string    `json:"url"`
+	DateCreated time.Time `json:"dateCreated"`
+	Pin         string    `json:"pin"`
 }
 
-func createURL(url string) shortURL {
-	newURL := shortURL{
-		key:         generateKey(),
-		url:         url,
-		dateCreated: time.Now(),
-		pin:         "1234",
+func createURL(url string, db *sql.DB) shortURL {
+	return shortURL{
+		ShortURL:    generateKey(db),
+		Url:         url,
+		DateCreated: time.Now(),
+		Pin:         generatePin(),
 	}
-	return newURL
 }
 
-func handleURL(w http.ResponseWriter, r *http.Request) {
+func handleNewURL(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 	res := flattenForm(r.Form)
 	if res["url"] == "" {
 		http.Error(w, "url is required", http.StatusBadRequest)
+		return
 	}
-	_, err := checkURLValid(res["url"])
-	if err != nil {
-		http.Error(w, "url is invalid or unreachable", http.StatusBadRequest)
+	url := res["url"]
+	if strings.HasPrefix(url, "https://") == false && strings.HasPrefix(url, "http://") == false {
+		url = "http://" + url
 	}
-	newURL := createURL(res["url"])
 
-	w.Write([]byte(newURL.key))
+	_, err := checkURLValid(url)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	newURL := createURL(url, db)
+
+	stmt, err := db.Prepare("INSERT INTO urls (name, redirect_url, date_created, pin) VALUES ($1, $2, $3, $4)")
+	if err != nil {
+		http.Error(w, "Error inserting into database", http.StatusInternalServerError)
+		return
+	}
+	_, err = stmt.Exec(newURL.ShortURL, newURL.Url, newURL.DateCreated, newURL.Pin)
+	if err != nil {
+		http.Error(w, "Error inserting into database", http.StatusInternalServerError)
+		return
+	}
+
+	jsonUrl, err := json.Marshal(newURL)
+	if err != nil {
+		http.Error(w, "Error generating JSON", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(jsonUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func checkURLValid(url string) (bool, error) {
@@ -49,7 +84,6 @@ func checkURLValid(url string) (bool, error) {
 	if err != nil {
 		return false, fmt.Errorf("error matching url: %v", err)
 	}
-	// make a get request to the url and check if the response status is 100
 	get, err := http.Get(url)
 	if err != nil {
 		return false, fmt.Errorf("error making get request: %v", err)
@@ -60,10 +94,101 @@ func checkURLValid(url string) (bool, error) {
 	return val, nil
 }
 
+func initDB() *sql.DB {
+	connStr := "user=" + os.Getenv("username") + " dbname=shorty" + " password=" + os.Getenv("password") + " sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
 func main() {
-	fmt.Println("Starting server...")
-	http.HandleFunc("/url", handleURL)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	db := initDB()
+	http.HandleFunc("/{path...}", func(w http.ResponseWriter, r *http.Request) {
+		handleURL(w, r, db)
+	})
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db)
+
+}
+
+func handleURL(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if r.URL.Path == "/new" {
+		handleNewURL(w, r, db)
+		return
+	} else if r.URL.Path == "/delete" {
+		handleDeleteURL(w, r, db)
+		return
+	} else {
+		//searchAndRedirect(w, r, db)
+		return
+	}
+
+}
+
+func handleDeleteURL(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if r.Method != "DELETE" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	res := flattenForm(r.Form)
+	if res["pin"] == "" {
+		http.Error(w, "Pin is required", http.StatusBadRequest)
+	}
+	pin := res["pin"]
+	name := res["name"]
+	stmt, err := db.Prepare("SELECT * FROM urls WHERE name = $1")
+	if err != nil {
+		http.Error(w, "Error selecting from database", http.StatusInternalServerError)
+		return
+	}
+	rows, err := stmt.Query(name)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.Error(w, "URL not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Error selecting from database", http.StatusInternalServerError)
+	}
+	var url shortURL
+	if rows == nil {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	}
+	for rows.Next() {
+		err = rows.Scan(&url.ShortURL, &url.Url, &url.DateCreated, &url.Pin)
+		if err != nil {
+			http.Error(w, "Error scanning from database", http.StatusInternalServerError)
+			return
+		}
+		if url.Pin == pin {
+			stmt, err := db.Prepare("DELETE FROM urls WHERE name = $1 AND pin = $2")
+			if err != nil {
+				http.Error(w, "Error deleting from database", http.StatusInternalServerError)
+				return
+			}
+			_, err = stmt.Exec(url.ShortURL, pin)
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
+	http.Error(w, "Invalid pin", http.StatusBadRequest)
+
 }
--- utils.go
+++ utils.go
@@ -1,6 +1,10 @@
 package main
 
-import "math/rand"
+import (
+	"database/sql"
+	"log"
+	"math/rand"
+)
 
 func flattenForm(form map[string][]string) map[string]string {
 	flatMap := make(map[string]string)
@@ -12,11 +16,39 @@ func flattenForm(form map[string][]string) map[string]string {
 	return flatMap
 }
 
-func generateKey() string {
+func generateKey(db *sql.DB) string {
+	prohibited := map[string]bool{"new": true, "delete": true}
 	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 	key := ""
-	for i := 0; i < 6; i++ {
-		key += string(chars[rand.Intn(len(chars))])
+
+	for {
+		for i := 0; i < 6; i++ {
+			key += string(chars[rand.Intn(len(chars))])
+		}
+		if prohibited[key] {
+			continue
+		}
+		stmt, err := db.Prepare("SELECT count(name) FROM urls where name = $1")
+		if err != nil {
+			log.Fatal(err)
+		}
+		row := stmt.QueryRow(key)
+		var count int
+		err = row.Scan(&count)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if count == 0 {
+			return key
+		}
 	}
-	return key
+}
+
+func generatePin() string {
+	chars := "1234567890"
+	pin := ""
+	for i := 0; i < 8; i++ {
+		pin += string(chars[rand.Intn(len(chars))])
+	}
+	return pin
 }
